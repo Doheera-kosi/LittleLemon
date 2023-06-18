95c019e383568c03797399f87990fa6c0d1de519
Doheera-kosi <kupourevans99@gmail.com>
2023-06-18 11:00:02 +0000

    Configured app to use mysql database

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#Settings.py 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'LittleLemon',
+         'HOST': '127.0.0.1',
+         'PORT': '3306',
+         'USER': 'root',
+         'PASSWORD': 'super',
+         'OPTIONS': {
+             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
