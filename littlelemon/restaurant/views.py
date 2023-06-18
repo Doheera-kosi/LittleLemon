525bd8459b16b5c2cef7d38d2ab98cb1fd4f5038
Doheera-kosi <kupourevans99@gmail.com>
2023-06-18 23:28:05 +0000

    Build Menu API in the Restaurant App

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
+
+
+# MENUITEM
+class MenuItemView(ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+  
+# SingleMenuItemView
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
