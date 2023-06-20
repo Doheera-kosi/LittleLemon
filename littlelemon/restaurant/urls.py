465acd768741f2c6231903ac949d272adabddf08
Doheera-kosi <kupourevans99@gmail.com>
2023-06-20 10:10:55 +0000

    Update restaurant/urls.py

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu/', views.MenuItemView.as_view()),
+    path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('menu/items/', views.MenuItemsView.as_view(), name='menu-items'),
 ]
