2fd6b6300426dadc0851ed8e40bc37f42613dbcb
Doheera-kosi <kupourevans99@gmail.com>
2023-06-21 14:46:41 +0000

    modify views.py

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,34 +1,3 @@
-# from django.shortcuts import render
-# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-# from rest_framework import viewsets
-# from rest_framework import generics
-
-# from .models import Menu, Booking
-# from .serializers import MenuSerializer, BookingSerializer
-# from rest_framework.permissions import IsAuthenticated
-
-# # Create your views here.
-# def index(request):
-#   return render(request, 'index.html', {})
-
-
-# # MENUITEM
-# class MenuItemsView(ListCreateAPIView):
-#    permission_classes = [IsAuthenticated]
-#    queryset = Menu.objects.all()
-#    serializer_class = MenuSerializer
-
-  
-# # SingleMenuItemView
-# class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
-#   queryset = Menu.objects.all()
-#   serializer_class = MenuSerializer
-
-# class BookingViewSet(viewsets.ModelViewSet):
-#   permission_classes = [IsAuthenticated]
-#   queryset = Booking.objects.all()
-#   serializer_class = BookingSerializer
-
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
